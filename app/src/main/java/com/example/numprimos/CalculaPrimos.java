24a3cf8218253040d3398b33de6d104648ce450c
nat-valerio <nat.valerio95@gmail.com>
2020-01-18 18:28:19 -0300

    Adicionando classe CalculaPrimos.

---
+++ app/src/main/java/com/example/numprimos/CalculaPrimos.java
@@ -0,0 +1,17 @@
+package com.example.numprimos;
+
+public class CalculaPrimos {
+
+    public Double CalculaPrimos(double resultado){
+        int num = 0;
+
+        resultado = num %1;
+
+        if(resultado == 1){
+
+        }
+
+
+        return resultado;
+    }
+}
